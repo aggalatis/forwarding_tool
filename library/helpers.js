ca7bcabc472c64015bbc266c0ca66ebeea0399b5
Aggelos Galatis <aggalatis52@gmail.com>
2021-12-19 09:28:35 +0000

    Fixed user can consolidate only thei jobs

--- library/helpers.js
+++ library/helpers.js
@@ -1,332 +1,291 @@
 let HelpersClass = function () {
-  $.fn.modal.Constructor.prototype._enforceFocus = function () {};
-  console.log("Constructing Helpers...");
-  this.user_username = "";
-  this.user_fullname = "";
-  this.user_id = "";
-  this.user_role_id = "";
-};
+    $.fn.modal.Constructor.prototype._enforceFocus = function () {}
+    console.log('Constructing Helpers...')
+    this.user_username = ''
+    this.user_fullname = ''
+    this.user_id = ''
+    this.user_role_id = ''
+}
 
 HelpersClass.prototype.bindMovingEvents = function (elementID) {
-  elementID = "#" + elementID;
-  $(".move-left").on("click", function () {
-    if ($(elementID).hasClass("floatMeRight")) {
-      $(elementID).removeClass("floatMeRight").addClass("noFloat");
-    } else if ($(elementID).hasClass("noFloat")) {
-      $(elementID).removeClass("noFloat").addClass("floatMeLeft");
-    } else {
-      $(elementID).addClass("floatMeLeft");
-    }
-  });
+    elementID = '#' + elementID
+    $('.move-left').on('click', function () {
+        if ($(elementID).hasClass('floatMeRight')) {
+            $(elementID).removeClass('floatMeRight').addClass('noFloat')
+        } else if ($(elementID).hasClass('noFloat')) {
+            $(elementID).removeClass('noFloat').addClass('floatMeLeft')
+        } else {
+            $(elementID).addClass('floatMeLeft')
+        }
+    })
 
-  $(".move-right").on("click", function () {
-    if ($(elementID).hasClass("floatMeLeft")) {
-      $(elementID).removeClass("floatMeLeft").addClass("noFloat");
-    } else if ($(elementID).hasClass("noFloat")) {
-      $(elementID).removeClass("noFloat").addClass("floatMeRight");
-    } else {
-      $(elementID).addClass("floatMeRight");
-    }
-  });
-};
+    $('.move-right').on('click', function () {
+        if ($(elementID).hasClass('floatMeLeft')) {
+            $(elementID).removeClass('floatMeLeft').addClass('noFloat')
+        } else if ($(elementID).hasClass('noFloat')) {
+            $(elementID).removeClass('noFloat').addClass('floatMeRight')
+        } else {
+            $(elementID).addClass('floatMeRight')
+        }
+    })
+}
 HelpersClass.prototype.toastr = function (type, message) {
-  // notification popup
-  toastr.options.closeButton = true;
-  toastr.options.positionClass = "toast-top-right";
-  toastr.options.showDuration = 1000;
-  toastr[type](message);
-};
+    // notification popup
+    toastr.options.closeButton = true
+    toastr.options.positionClass = 'toast-top-right'
+    toastr.options.showDuration = 1000
+    toastr[type](message)
+}
 
 HelpersClass.prototype.handleLogout = function () {
-  ipcRenderer.send("restart");
-};
+    ipcRenderer.send('restart')
+}
 
 HelpersClass.prototype.saveUserData = function (userData) {
-  let fs = require("fs");
-  var userString = "";
-  userString =
-    userData.user_username +
-    ";" +
-    userData.user_fullname +
-    ";" +
-    userData.user_id +
-    ";" +
-    userData.user_role_id;
+    let fs = require('fs')
+    var userString = ''
+    userString = userData.user_username + ';' + userData.user_fullname + ';' + userData.user_id + ';' + userData.user_role_id
 
-  fs.writeFile("C:\\ForwardTool\\usrdata.agcfg", userString, function (err) {
-    if (err) throw err;
-  });
-};
+    fs.writeFile('./usrdata.agcfg', userString, function (err) {
+        if (err) throw err
+    })
+}
 
 HelpersClass.prototype.initializeUser = function () {
-  let self = this;
+    let self = this
 
-  let fs = require("fs");
-  console.log("Initialiazing User.....");
-  var userFile = fs.readFileSync("C:\\ForwardTool\\usrdata.agcfg", "utf8");
+    let fs = require('fs')
+    console.log('Initialiazing User.....')
+    var userFile = fs.readFileSync('./usrdata.agcfg', 'utf8')
 
-  var userFileData = userFile.split(";");
-  self.user_username = userFileData[0];
-  self.user_fullname = userFileData[1];
-  self.user_id = userFileData[2];
-  self.user_role_id = userFileData[3];
-  $("#span-username").html(userFileData[1]);
+    var userFileData = userFile.split(';')
+    self.user_username = userFileData[0]
+    self.user_fullname = userFileData[1]
+    self.user_id = userFileData[2]
+    self.user_role_id = userFileData[3]
+    $('#span-username').html(userFileData[1])
 
-  if (userFileData[3] != 1) {
-    $("#statistics_list").attr("hidden", null);
-    $("#deleted-ind").attr("hidden", null);
-    $("#deleted-person").attr("hidden", null);
-    $("#add_data_list").attr("hidden", null);
-    $("#vessel-option").show();
-  }
-};
+    if (userFileData[3] != 1) {
+        $('#statistics_list').attr('hidden', null)
+        $('#deleted-ind').attr('hidden', null)
+        $('#deleted-person').attr('hidden', null)
+        $('#add_data_list').attr('hidden', null)
+        $('#vessel-option').show()
+    }
+}
 
 HelpersClass.prototype.getDateTimeNow = function () {
-  var datetime = require("datetime-js");
-  var dateObj = new Date();
+    var datetime = require('datetime-js')
+    var dateObj = new Date()
 
-  return datetime(dateObj, "%Y-%m-%d %H:%i:%s");
-};
+    return datetime(dateObj, '%Y-%m-%d %H:%i:%s')
+}
 
 HelpersClass.prototype.changeDateToMysql = function (datetime) {
-  var dateTimeArray = datetime.split("/");
-  if (dateTimeArray.length == 3) {
-    return dateTimeArray[2] + "-" + dateTimeArray[1] + "-" + dateTimeArray[0];
-  } else {
-    return datetime;
-  }
-};
+    var dateTimeArray = datetime.split('/')
+    if (dateTimeArray.length == 3) {
+        return dateTimeArray[2] + '-' + dateTimeArray[1] + '-' + dateTimeArray[0]
+    } else {
+        return datetime
+    }
+}
 
 HelpersClass.prototype.changeMysqlDateToNormal = function (datetime) {
-  var dateTimeArray = datetime.split(" ");
-  let dateSplitted = dateTimeArray[0].split("-");
-  return `${dateSplitted[2]}/${dateSplitted[1]}/${dateSplitted[0]} ${dateTimeArray[1]}`;
-};
+    var dateTimeArray = datetime.split(' ')
+    let dateSplitted = dateTimeArray[0].split('-')
+    return `${dateSplitted[2]}/${dateSplitted[1]}/${dateSplitted[0]} ${dateTimeArray[1]}`
+}
 
-HelpersClass.prototype.initliazeModalToEditJob = function (
-  divisions,
-  products,
-  vessels,
-  cities,
-  jobData
-) {
-  let self = this;
-  myTransfers.initializeDivisionsSelect();
-  self.findChoosenValueForDivision(divisions, jobData[3]);
-  myTransfers.initializeProductsSelect(divisions[i].division_id);
-  self.findChoosenValueForProducts(products, jobData[4]);
-  myTransfers.initialiazeVesselsSelect();
-  self.findChoosenValueForVessels(vessels, jobData[6]);
-  myTransfers.initialiazeCitiesSelect();
-  self.findChoosenValueForCities(cities, jobData[7], jobData[8]);
+HelpersClass.prototype.initliazeModalToEditJob = function (divisions, products, vessels, cities, jobData) {
+    let self = this
+    myTransfers.initializeDivisionsSelect()
+    self.findChoosenValueForDivision(divisions, jobData[3])
+    myTransfers.initializeProductsSelect(divisions[i].division_id)
+    self.findChoosenValueForProducts(products, jobData[4])
+    myTransfers.initialiazeVesselsSelect()
+    self.findChoosenValueForVessels(vessels, jobData[6])
+    myTransfers.initialiazeCitiesSelect()
+    self.findChoosenValueForCities(cities, jobData[7], jobData[8])
 
-  $("#modal-title-text").html("Edit Job");
-  $("#mode-select").val(jobData[5]);
-  $("#mode-select").trigger("chosen:updated");
-  $("#deadline_date").val(jobData[9]);
-  $("#forwarder").val(jobData[10]);
-  $("#reference").val(jobData[11]);
-  $("#kg").val(jobData[12]);
-  if (jobData[12] == "0") {
-    $("#kg").val("");
-  } else {
-    $("#kg").val(jobData[12]);
-  }
-
-  if (jobData[13] == "0") {
-    $("#estimate_cost").val("");
-  } else {
-    $("#estimate_cost").val(jobData[13]);
-  }
-  $("#notes").val(jobData[14]);
-
-  $("#save-job-btn").attr("disabled", null);
-  $("#add-job-modal").modal("show");
-};
-
-HelpersClass.prototype.initliazeModalToEditConsolidation = function (
-  divisions,
-  ports,
-  products,
-  vessels,
-  cities,
-  jobData
-) {
-  let self = this;
-
-  myConsolidations.initializeDivisionsSelect();
-  self.findChoosenValueForDivision(divisions, jobData[2]);
-  myConsolidations.initializeProductsSelect(divisions[i].division_id);
-  self.findChoosenValueForProducts(products, jobData[3]);
-  myConsolidations.initialiazeVesselsSelect();
-  self.findChoosenValueForVessels(vessels, jobData[15]);
-  myConsolidations.initialiazeCitiesSelect();
-
-  $("#modal-title-text").html("Edit Consolidation");
-  $("#mode-select").val(jobData[4]);
-  $("#mode-select").trigger("chosen:updated");
-  $("#deadline_date").val(jobData[19]);
-  $("#cutoff_date").val(jobData[20]);
-  $("#forwarder").val(jobData[22]);
-  $("#carrier").val(jobData[5]);
-  $("#notes").val(jobData[23]);
-  $("#reference").val(jobData[6]);
-  $("#actual_weight").val(jobData[7]);
-  $("#length").val(jobData[8]);
-  $("#width").val(jobData[9]);
-  $("#height").val(jobData[10]);
-  $("#pieces").val(jobData[11]);
-  $("#volume_weight").val(jobData[12]);
-
-  $("#ex-input").val(jobData[16]).trigger("chosen:updated");
-  $("#to-input").val(jobData[17]).trigger("chosen:updated");
-
-  $("#save-job-btn").attr("disabled", null);
-
-  $("#add-consolidation-modal").modal("show");
-};
-
-HelpersClass.prototype.initliazeModalToEditPersonnel = function (
-  divisions,
-  products,
-  vessels,
-  cities,
-  jobData
-) {
-  let self = this;
-  myPersonnel.initializeDivisionsSelect();
-  self.findChoosenValueForDivision(divisions, jobData[3]);
-  myPersonnel.initializeProductsSelect(divisions[i].division_id);
-  self.findChoosenValueForProducts(products, jobData[4]);
-  myPersonnel.initialiazeVesselsSelect();
-  self.findChoosenValueForVessels(vessels, jobData[7]);
-  myPersonnel.initialiazeCitiesSelect();
-  self.findChoosenValueForCities(cities, jobData[8], jobData[9]);
-
-  $("#modal-title-text").html("Edit Personnel");
-  $("#mode-select").val(jobData[5]);
-  $("#mode-select").trigger("chosen:updated");
-  $("#deadline_date").val(jobData[10]);
-  $("#name").val(jobData[6]);
-  $("#kg").val(jobData[11]);
-  $("#estimate_cost").val(jobData[12]);
-  $("#actual_cost").val(jobData[13]);
-  $("#saving").val(jobData[14]);
-  $("#notes").val(jobData[15]);
-
-  $("#save-per-btn").attr("disabled", null);
-  $("#add-per-modal").modal("show");
-};
-
-HelpersClass.prototype.findChoosenValueForDivision = function (
-  divisions,
-  division_description
-) {
-  let self = this;
-
-  for (i = 0; i < divisions.length; i++) {
-    if (divisions[i].division_description == division_description) {
-      $("#division-select").val(divisions[i].division_id);
-      $("#division-select").trigger("chosen:updated");
-      break;
-    }
-  }
-};
-
-HelpersClass.prototype.findChoosenValueForProducts = function (
-  products,
-  product_names
-) {
-  let self = this;
-
-  var productNames = product_names.split(";");
-  var productIds = [];
-  for (i = 0; i < products.length; i++) {
-    if (
-      productNames.indexOf(products[i].product_description.toString()) !== -1
-    ) {
-      productIds.push(products[i].product_id);
-    }
-  }
-  $("#product-select").val(productIds);
-  $("#product-select").trigger("chosen:updated");
-};
-
-HelpersClass.prototype.findChoosenValueForVessels = function (
-  vessels,
-  vessels_description
-) {
-  let self = this;
-
-  var vesselNames = vessels_description.split(";");
-  var vesselIds = [];
-  for (i = 0; i < vessels.length; i++) {
-    if (vesselNames.indexOf(vessels[i].vessel_description.toString()) !== -1) {
-      vesselIds.push(vessels[i].vessel_id);
-    }
-  }
-  $("#vessel-select").val(vesselIds);
-  $("#vessel-select").trigger("chosen:updated");
-};
-
-HelpersClass.prototype.findChoosenValueForCities = function (
-  cities,
-  ex_city,
-  to_city
-) {
-  let self = this;
-  for (i = 0; i < cities.length; i++) {
-    if (cities[i].city_name == ex_city) {
-      $("#ex-input").val(cities[i].city_id);
-      $("#ex-input").trigger("chosen:updated");
-    }
-    if (cities[i].city_name == to_city) {
-      $("#to-input").val(cities[i].city_id);
-      $("#to-input").trigger("chosen:updated");
-    }
-  }
-};
-HelpersClass.prototype.formatFloatValue = function (num) {
-  if (num.includes(".")) {
-    var slpittedNum = num.split(".");
-
-    if (slpittedNum[1].length > 1) {
-      return num;
+    $('#modal-title-text').html('Edit Job')
+    $('#mode-select').val(jobData[5])
+    $('#mode-select').trigger('chosen:updated')
+    $('#deadline_date').val(jobData[9])
+    $('#forwarder').val(jobData[10])
+    $('#reference').val(jobData[11])
+    $('#kg').val(jobData[12])
+    if (jobData[12] == '0') {
+        $('#kg').val('')
     } else {
-      return num + "0";
+        $('#kg').val(jobData[12])
     }
-  } else if (num !== "" && num !== "null") {
-    return num + ".00";
-  } else {
-    return 0;
-  }
-};
+
+    if (jobData[13] == '0') {
+        $('#estimate_cost').val('')
+    } else {
+        $('#estimate_cost').val(jobData[13])
+    }
+    $('#notes').val(jobData[14])
+
+    $('#save-job-btn').attr('disabled', null)
+    $('#add-job-modal').modal('show')
+}
+
+HelpersClass.prototype.initliazeModalToEditConsolidation = function (divisions, ports, products, vessels, cities, jobData) {
+    let self = this
+
+    myConsolidations.initializeDivisionsSelect()
+    self.findChoosenValueForDivision(divisions, jobData[2])
+    myConsolidations.initializeProductsSelect(divisions[i].division_id)
+    self.findChoosenValueForProducts(products, jobData[3])
+    myConsolidations.initialiazeVesselsSelect()
+    self.findChoosenValueForVessels(vessels, jobData[15])
+    myConsolidations.initialiazeCitiesSelect()
+
+    $('#modal-title-text').html('Edit Consolidation')
+    $('#mode-select').val(jobData[4])
+    $('#mode-select').trigger('chosen:updated')
+    $('#deadline_date').val(jobData[19])
+    $('#cutoff_date').val(jobData[20])
+    $('#forwarder').val(jobData[22])
+    $('#carrier').val(jobData[5])
+    $('#notes').val(jobData[23])
+    $('#reference').val(jobData[6])
+    $('#actual_weight').val(jobData[7])
+    $('#length').val(jobData[8])
+    $('#width').val(jobData[9])
+    $('#height').val(jobData[10])
+    $('#pieces').val(jobData[11])
+    $('#volume_weight').val(jobData[12])
+
+    $('#ex-input').val(jobData[16]).trigger('chosen:updated')
+    $('#to-input').val(jobData[17]).trigger('chosen:updated')
+
+    $('#save-job-btn').attr('disabled', null)
+
+    $('#add-consolidation-modal').modal('show')
+}
+
+HelpersClass.prototype.initliazeModalToEditPersonnel = function (divisions, products, vessels, cities, jobData) {
+    let self = this
+    myPersonnel.initializeDivisionsSelect()
+    self.findChoosenValueForDivision(divisions, jobData[3])
+    myPersonnel.initializeProductsSelect(divisions[i].division_id)
+    self.findChoosenValueForProducts(products, jobData[4])
+    myPersonnel.initialiazeVesselsSelect()
+    self.findChoosenValueForVessels(vessels, jobData[7])
+    myPersonnel.initialiazeCitiesSelect()
+    self.findChoosenValueForCities(cities, jobData[8], jobData[9])
+
+    $('#modal-title-text').html('Edit Personnel')
+    $('#mode-select').val(jobData[5])
+    $('#mode-select').trigger('chosen:updated')
+    $('#deadline_date').val(jobData[10])
+    $('#name').val(jobData[6])
+    $('#kg').val(jobData[11])
+    $('#estimate_cost').val(jobData[12])
+    $('#actual_cost').val(jobData[13])
+    $('#saving').val(jobData[14])
+    $('#notes').val(jobData[15])
+
+    $('#save-per-btn').attr('disabled', null)
+    $('#add-per-modal').modal('show')
+}
+
+HelpersClass.prototype.findChoosenValueForDivision = function (divisions, division_description) {
+    let self = this
+
+    for (i = 0; i < divisions.length; i++) {
+        if (divisions[i].division_description == division_description) {
+            $('#division-select').val(divisions[i].division_id)
+            $('#division-select').trigger('chosen:updated')
+            break
+        }
+    }
+}
+
+HelpersClass.prototype.findChoosenValueForProducts = function (products, product_names) {
+    let self = this
+
+    var productNames = product_names.split(';')
+    var productIds = []
+    for (i = 0; i < products.length; i++) {
+        if (productNames.indexOf(products[i].product_description.toString()) !== -1) {
+            productIds.push(products[i].product_id)
+        }
+    }
+    $('#product-select').val(productIds)
+    $('#product-select').trigger('chosen:updated')
+}
+
+HelpersClass.prototype.findChoosenValueForVessels = function (vessels, vessels_description) {
+    let self = this
+
+    var vesselNames = vessels_description.split(';')
+    var vesselIds = []
+    for (i = 0; i < vessels.length; i++) {
+        if (vesselNames.indexOf(vessels[i].vessel_description.toString()) !== -1) {
+            vesselIds.push(vessels[i].vessel_id)
+        }
+    }
+    $('#vessel-select').val(vesselIds)
+    $('#vessel-select').trigger('chosen:updated')
+}
+
+HelpersClass.prototype.findChoosenValueForCities = function (cities, ex_city, to_city) {
+    let self = this
+    for (i = 0; i < cities.length; i++) {
+        if (cities[i].city_name == ex_city) {
+            $('#ex-input').val(cities[i].city_id)
+            $('#ex-input').trigger('chosen:updated')
+        }
+        if (cities[i].city_name == to_city) {
+            $('#to-input').val(cities[i].city_id)
+            $('#to-input').trigger('chosen:updated')
+        }
+    }
+}
+HelpersClass.prototype.formatFloatValue = function (num) {
+    if (num.includes('.')) {
+        var slpittedNum = num.split('.')
+
+        if (slpittedNum[1].length > 1) {
+            return num
+        } else {
+            return num + '0'
+        }
+    } else if (num !== '' && num !== 'null') {
+        return num + '.00'
+    } else {
+        return 0
+    }
+}
 
 HelpersClass.prototype.addCityAlert = function (myDB) {
-  let self = this;
-  Swal.fire({
-    title: "Create a new city",
-    input: "text",
-    inputLabel: "Type a name for your city.",
-    inputPlaceholder: "City name",
-    showCancelButton: true,
-    showConfirmButton: true,
-    inputValidator: (value) => {
-      if (!value) {
-        return "Please type city name first..";
-      }
-    },
-  }).then((cityObj) => {
-    if (cityObj.isConfirmed) {
-      myDB.addCity(cityObj.value);
-    }
-  });
-};
+    let self = this
+    Swal.fire({
+        title: 'Create a new city',
+        input: 'text',
+        inputLabel: 'Type a name for your city.',
+        inputPlaceholder: 'City name',
+        showCancelButton: true,
+        showConfirmButton: true,
+        inputValidator: value => {
+            if (!value) {
+                return 'Please type city name first..'
+            }
+        },
+    }).then(cityObj => {
+        if (cityObj.isConfirmed) {
+            myDB.addCity(cityObj.value)
+        }
+    })
+}
 
 HelpersClass.prototype.checkIfUserHasPriviledges = function (jobUserName) {
-  let self = this;
-  if (jobUserName == self.user_username || self.user_role_id == 2) {
-    return true;
-  }
-  return false;
-};
+    let self = this
+    if (jobUserName == self.user_username || self.user_role_id == 2) {
+        return true
+    }
+    return false
+}

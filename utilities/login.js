32c6f98d09b70167dfd35815190f3f343b69aab0
AG <aggalatis52@gmail.com>
2022-04-27 22:35:46 +0300

    Added iso message on login page

--- utilities/login.js
+++ utilities/login.js
@@ -1,49 +1,49 @@
 let LoginClass = function () {
+    this.DB = new DbClass()
+    this.Helpers = new HelpersClass()
+    this.submitFormOnEnter()
+    $('#iso-read-more').on('click', function () {
+        var dots = document.getElementById('dots')
+        var moreText = document.getElementById('more')
+        var btnText = document.getElementById('iso-read-more')
 
-    this.DB = new DbClass();
-    this.Helpers = new HelpersClass();
-    this.submitFormOnEnter();
-
-
+        if (dots.style.display === 'none') {
+            dots.style.display = 'inline'
+            btnText.innerHTML = 'Read more'
+            moreText.style.display = 'none'
+        } else {
+            dots.style.display = 'none'
+            btnText.innerHTML = 'Read less'
+            moreText.style.display = 'inline'
+        }
+    })
 }
 
-LoginClass.prototype.submitLoginForm = function() {
+LoginClass.prototype.submitLoginForm = function () {
+    let self = this
+    var username = $('#user_username').val()
+    var password = $('#user_password').val()
 
-    let self =  this;
-    var username = $('#user_username').val();
-    var password = $('#user_password').val();
-
-
-    self.DB.verifyLogin(username, password);
-
-
+    self.DB.verifyLogin(username, password)
 }
 
-LoginClass.prototype.submitFormOnEnter = function() {
-
-    let self =  this;
-
+LoginClass.prototype.submitFormOnEnter = function () {
+    let self = this
     $('#user_username').focus()
 
-    $('#user_username').on("keyup", function(event) {
-
+    $('#user_username').on('keyup', function (event) {
         if (event.keyCode === 13) {
-
-            event.preventDefault();
+            event.preventDefault()
 
             $('#sign-in-btn').trigger('click')
         }
-    });
+    })
 
-    $('#user_password').on("keyup", function(event) {
-
+    $('#user_password').on('keyup', function (event) {
         if (event.keyCode === 13) {
-
-            event.preventDefault();
+            event.preventDefault()
 
             $('#sign-in-btn').trigger('click')
         }
-    });
-
-
+    })
 }
